f3a63cf677d1546cbab5bcef1c8d39a6203a6dde
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-04-18 20:19:56 -0300

    Projeto Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalcularConsumoComponent } from './calcular-consumo/calcular-consumo.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask, IConfig } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
   ],
-  providers: [],
+  providers: [provideNgxMask()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/calcular-consumo/calcular-consumo.component.ts
+++ src/app/calcular-consumo/calcular-consumo.component.ts
@@ -4,7 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-calcular-consumo',
   templateUrl: './calcular-consumo.component.html',
   styleUrls: ['./calcular-consumo.component.css']
+  
 })
 export class CalcularConsumoComponent {
 
+  quantPessoas : number = 0;
+  quantComodos : number = 0;
+  quantTVs : number = 0;
+  quantPCs : number = 0;
+  valorTarifa : number = 0;
+
+  energiaGasta : number = 0;
+  valorContaLuz : number = 0;
+
+  calcularGasto(){
+    let gastoChuveiro : number = ((5400*0.1666*30)/1000)*this.quantPessoas;
+    let gastoTV : number = ((150*6*30)/1000)*this.quantTVs;
+    let gastoPC : number = ((250*8*30)/1000)*this.quantPCs;
+    let gastoLampada : number = ((15*5*30)/1000)*this.quantComodos;
+    let gastoMaquinaLavar : number = 0;
+    let gastoSecadora : number = 0;
+
+    let temMaquina = document.getElementById("checkMaquina") as HTMLInputElement;
+    if(temMaquina.checked){
+      gastoMaquinaLavar = ((1200*2*10)/1000);
+    }
+    let temSecadora = document.getElementById("checkSecadora") as HTMLInputElement;
+    if(temSecadora.checked){
+      gastoSecadora = ((2000*1*10)/1000);
+    }
+
+    this.energiaGasta = parseFloat((gastoChuveiro + gastoTV + gastoPC + gastoLampada + gastoMaquinaLavar + gastoSecadora).toFixed(2));
+    this.valorContaLuz = parseFloat((this.energiaGasta * this.valorTarifa).toFixed(2));
+
+  }
 }
